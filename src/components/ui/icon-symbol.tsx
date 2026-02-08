addea6d0fab7226e7fc14669a4d0384d5ce1e845
zac rabhi <zacrabhi@zacs-MacBook-Pro.local>
2026-02-08 18:35:38 +0100

    add folder aliases for cleaner and more readable imports

--- src/components/ui/icon-symbol.tsx
+++ src/components/ui/icon-symbol.tsx
@@ -1,6 +1,6 @@
 // Fallback for using MaterialIcons on Android and web.
 
-import MaterialIcons from '@/srcexpo/vector-icons/MaterialIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { SymbolViewProps, SymbolWeight } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
